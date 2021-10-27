db1ff9a0aba6095ab92855b322d973bf80539316
Toto <70719309+totochow@users.noreply.github.com>
2021-10-27 10:43:03 -0700

    Update 202110270954 - Reading csv file into SQL.sql

--- 202110270954 - Reading csv file into SQL.sql
+++ 202110270954 - Reading csv file into SQL.sql
@@ -13,55 +13,15 @@ GO
 DROP TABLE IF EXISTS #temptable;
 GO
 
+-- rename "ColName" to your desired column names
+
 create table #temptable  (	ID int IDENTITY, 
-						ColName01 varchar(100), 
-						ColName02 varchar(100), 
-						ColName03 varchar(100), 
-						ColName04 varchar(100), 
-						ColName05 varchar(100), 
-						ColName06 varchar(100), 
-						ColName07 varchar(100), 
-						ColName08 varchar(100), 
-						ColName09 varchar(100), 
-						ColName10 varchar(100), 
-						ColName11 varchar(100), 
-						ColName12 varchar(100), 
-						ColName13 varchar(100), 
-						ColName14 varchar(100), 
-						ColName15 varchar(100), 
-						ColName16 varchar(100), 
-						ColName17 varchar(100), 
-						ColName18 varchar(100), 
-						ColName19 varchar(100), 
-						ColName20 varchar(100), 
-						ColName21 varchar(100), 
-						ColName22 varchar(100), 
-						ColName23 varchar(100), 
-						ColName24 varchar(100), 
-						ColName25 varchar(100), 
-						ColName26 varchar(100), 
-						ColName27 varchar(100), 
-						ColName28 varchar(100), 
-						ColName29 varchar(100), 
-						ColName30 varchar(100), 
-						ColName31 varchar(100), 
-						ColName32 varchar(100), 
-						ColName33 varchar(100), 
-						ColName34 varchar(100), 
-						ColName35 varchar(100), 
-						ColName36 varchar(100), 
-						ColName37 varchar(100), 
-						ColName38 varchar(100), 
-						ColName39 varchar(100), 
-						ColName40 varchar(100), 
-						ColName41 varchar(100), 
-						ColName42 varchar(100), 
-						ColName43 varchar(100), 
-						ColName44 varchar(100), 
-						ColName45 varchar(100), 
-						ColName46 varchar(100), 
-						ColName47 varchar(100), 
-						ColName48 varchar(100))
+				ColName01 varchar(100), 
+				ColName02 varchar(100), 
+				ColName03 varchar(100), 
+				ColName04 varchar(100), 
+					-- ...
+				ColNameN varchar(100))
 
 
 BULK INSERT #temptable
